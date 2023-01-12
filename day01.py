d5bdff8a21343f946f1672584ffc8e4fa1d4aa7b
kimhw <khw2023@gmail.com>
2023-01-12 16:38:48 +0900

    countdown 02

--- day01.py
+++ day01.py
@@ -1,4 +1,10 @@
-# (32°F − 32) × 5/9 = °C
-fahrenheit = float(input('화씨 온도 : '))
-celsius = (fahrenheit-32.0)*(5.0/9.0)
-print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다.')
+# 교재 countdown 예제
+# for countdown in 5,4,3,2,1,'hey!':
+#     print(countdown)
+# print('프로그램 종료')
+
+countdown_list = [5,4,3,2,1,'hey!']
+for countdown in countdown_list :
+     print(countdown)
+print(countdown_list[-3])
+print('프로그램 종료')
