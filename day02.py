924ab7feee3c12e3a2038bd94e0907d0b5a9f3ab
kimhw <rlagussdn2023@naver.com>
2023-01-13 13:37:50 +0900

    day02 mathvalues

--- day02.py
+++ day02.py
@@ -1,2 +1,9 @@
-PI = 3.14
-print(f'원주율은 {PI}이고 타입은 {type(PI)}다.')
+# math_values = (3.14,2.71)
+# print(f'원주율은 {math_values[0]}이고 타입은 {type(math_values)}다.')
+# math_values[0] = 9.99
+# print(f'원주율은 {math_values[0]}이고 타입은 {type(math_values)}다.')
+
+math_values = [3.14,2.71]
+print(f'원주율은 {math_values[0]}이고 타입은 {type(math_values)}다.')
+math_values[0] = 9.99
+print(f'원주율은 {math_values[0]}이고 타입은 {type(math_values)}다.')
