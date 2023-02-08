018ce2b9f22860955f23650f32178173f3badd13
kimhw <rlagussdn2023@naver.com>
2023-02-08 15:12:03 +0900

    day18_connect_check

--- day18.py
+++ day18.py
@@ -1,30 +1,45 @@
-## 함수 선언 부분 ##
+gSize = 6
 class Graph():
     def __init__(self, size):
         self.SIZE = size
         self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
 
-def printGraph(g):
-    print(' ', end=' ')
-    for v in range(g.SIZE):
-        print(nameAry[v], end=' ')
-    print()
-    for row in range(g.SIZE):
-        print(nameAry[row], end=' ')
-        for col in range(g.SIZE):
-            print(g.graph[row][col], end='  ')
-        print()
-    print()
+def findVertex(g, findVtx):
+    stack = []
+    visitedAry = []  # 방문한 노드
 
+    current = 0  # 시작 정점
+    stack.append(current)
+    visitedAry.append(current)
 
-## 전역 변수 선언 부분 ##
+    while (len(stack) != 0):
+        next = None
+        for vertex in range(gSize):
+            if g.graph[current][vertex] != 0:
+                if vertex in visitedAry:  # 방문한 적이 있는 정점이면 탈락
+                    pass
+                else:  # 방문한 적이 없으면 다음 정점으로 지정
+                    next = vertex
+                    break
+
+        if next != None:  # 다음에 방문할 정점이 있는 경우
+            current = next
+            stack.append(current)
+            visitedAry.append(current)
+        else:  # 다음에 방문할 정점이 없는 경우
+            current = stack.pop()
+
+    if findVtx in visitedAry:
+        return True
+    else:
+        return False
+
 G1 = None
 nameAry = ['문별', '솔라', '휘인', '쯔위', '선미', '화사']
 문별, 솔라, 휘인, 쯔위, 선미, 화사 = 0, 1, 2, 3, 4, 5
 stack = []
 visit = []
-## 메인 코드 부분 ##
 gSize = 6
 G1 = Graph(gSize)
 G1.graph[문별][솔라] = 1; G1.graph[문별][휘인] = 1
@@ -34,33 +49,4 @@ G1.graph[쯔위][솔라] = 1; G1.graph[쯔위][휘인] = 1; G1.graph[쯔위][선
 G1.graph[선미][쯔위] = 1; G1.graph[선미][화사] = 1
 G1.graph[화사][쯔위] = 1; G1.graph[화사][선미] = 1
 
-print('## G1 무방향 그래프 ##')
-printGraph(G1)
-
-current = 0
-stack.append(current)
-visit.append(current)
-
-while (len(stack) != 0):
-    next = None
-    for vertex in range(len(nameAry)):
-        if G1.graph[current][vertex] == 1:
-            if vertex in visit:
-                pass
-            else:
-                next = vertex
-                break
-    if next != None:
-        current = next
-        stack.append(current)
-        visit.append(current)
-    else:
-        current = stack.pop()
-
-print('방문순서 -->', end=' ')
-
-for i in visit:
-    print(nameAry[i], end=' ')
-
-
-
+print (findVertex(G1,화사))
